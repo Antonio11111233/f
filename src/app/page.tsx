a0d0c5dd1c94740516b489a390b7d9d4c2936f44
bfnde <gendalf.b3l1@gmail.com>
2024-05-02 17:09:36 +0600

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,117 @@
 import Image from "next/image";
+import Link from 'next/link'
+// import  MenuItem  from "./Home";
+import { MenuItem } from "./MenuItem";
+import { MenuItemTwo} from "./MenuItem-two"
+import { MENU } from "./menu.data";
+import { MENUTWO } from "./menu.data-two";
+import './globals.scss'
+import Particle from "./components/Particle";
+import './index.js';
+import { OctagonX } from "lucide-react";
+
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+  return (		
+    <main className="flex items-center h-screen relative bg-cover bg-[url('/assets/bg-explosion.png')]">
+      
+      <div className="absolute right-0 top-0 h-full w-[80%] z-[2]">
+        
+        <Particle/>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+      
+      <div className="absolute right-10 md:right-40 bottom-0 z-[10]/">
+        
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          src="/assets/MoustacheMan.png"
+          alt="MoustacheMan"
+          width={560}
+          height={560}
+          className="w-[300px] h-[300px] z-[1] md:h-[560px] md:w-[560px]"
         />
       </div>
+      <Image
+          src="/assets/top-left-img.png"
+          alt="paint"
+          width={230}
+          height={230}
+          className="absolute left-0 top-0"
+        />
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+        <div className="flex flex-col gap-3 z-[10] pl-40 pt-20">
+          <h1 className="flex-[50px] text-white max-w-[500px]">
+            Transformin ideas into{" "}
+            <span className="text-red-500">Digital Reality</span>
+          </h1>
+          <p className="text-[16px] text-gray-200 md:text-gray-400 mb-10 md:pb-2 max-w-[400px]">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+             Quibusdam ipsa, eligendi omnis atque voluptatum labore quas.
+              Atque itaque sit eveniet, esse culpa quam rerum vero officiis
+             ducimus a temporibus maiores.
+             
           </p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          <aside>
+        <div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+        <div className="btn-flip" data-back="Back" data-front="Индивидуальный" >
+          {MENU.map(item => (
+            <MenuItem
+              item={item}
+              key={item.link}
+            />
+          ))}
+        </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className="btn-flip" data-back="Релиз 2026" data-front="!В разработке!" >
+          {MENUTWO.map(item => (
+            <MenuItemTwo
+              item={item}
+              key={item.link}
+            />
+          ))}
+        </div>
+
+
+
+
+
+        {/* <div className="btn-flipt" data-back="Back" data-front="Индивидуальный">
+          <p>Командный</p>
+        </div>  */}
       </div>
+      </aside>
+        </div>
     </main>
+   
   );
-}
+};
+
+
+
+{/* <div className="relative w-[100px] h-[100px] md:w-[160] md:h-[160] mt-5"> */}
+{/* <Image
+    src="/assets/rounded-text.png"
+    alt="projects"
+    width={160}
+    height={160}
+    className="slow-spin"
+    
+/> */}
+
+{/* </div> */}
+
+
+
+  // /* <footer className='text-xs opacity-40 font-normal text-center p-layout'>
+  //   2024 &copy; With love from{' '}
+  //   <a
+  //     href='https://www.youtube.com/c/redgroup/?sub_confirmation=1'
+  //     target='_blank'
+  //     rel='noreferrer'
+  //     className='hover:text-primary text-brand-300 transition-colors'
+  //   >
+  //     RED Group
+  //   </a>
+  //   . <br /> All rights reserved.
+  // </footer> */
